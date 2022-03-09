bb5d45555e2b89816681756111067d5ed7905fb6
glitzerbargeld <torben.jaeckel@me.com>
2022-03-09 14:46:30 +0100

    removed condition

--- shop-customizations.php
+++ shop-customizations.php
@@ -10,10 +10,9 @@ add_action( 'woocommerce_after_shop_loop_item_title', 'custom_field_display_belo
 function custom_field_display_below_title(){
     global $product;
 
-    if(is_category( "cbd-blueten" )){
     // Display ACF text
     if( $text = get_field( 'anbaumethode', $product->get_id() ) ) {
         echo '<p class="anbaumethode">' . $text . '</p>';
     }
-    }
+    
 }
