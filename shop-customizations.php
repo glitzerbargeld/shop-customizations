544905859683c8abed8db928a6e6b16d97830879
glitzerbargeld <torben.jaeckel@me.com>
2022-03-10 11:46:33 +0100

    added cbdanteil icon

--- shop-customizations.php
+++ shop-customizations.php
@@ -13,6 +13,8 @@ add_action( 'woocommerce_after_shop_loop_item_title', 'custom_field_display_belo
 function custom_field_display_below_title(){
     global $product;
     // Display ACF text
+
+    echo '<div class="acf-infos">';
     if( $anbau = get_field( 'anbaumethode', $product->get_id() ) ) {
         echo '<span class="anbaumethode"><img src="' . plugin_dir_url(__FILE__) . 'icons/icon_' . strtolower($anbau) . '.svg' . '" width="30">' . $anbau . '</span>';
     }
@@ -20,5 +22,7 @@ function custom_field_display_below_title(){
     if( $text = get_field( 'cbd_gehalt', $product->get_id() ) ) {
         echo '<span class="cbd-anteil"><img src="' . plugin_dir_url(__FILE__) . 'icons/icon_cbd_gehalt.svg">'  . $text . '%</span>';
     }
+    
+   echo '</div>';
     
 }
