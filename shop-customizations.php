b3834de89ac02bae26cced5ab76c05ebd2fb9b43
glitzerbargeld <torben.jaeckel@me.com>
2022-03-11 10:15:08 +0100

    Add to cart button text change

--- shop-customizations.php
+++ shop-customizations.php
@@ -27,3 +27,10 @@ function custom_field_display_below_title(){
    echo '</div>';
     
 }
+add_filter( 'woocommerce_product_add_to_cart_text', function( $text ) {
+	global $product;
+	if ( $product->is_type( 'variable' ) ) {
+		$text = $product->is_purchasable() ? __( 'Zum Produkt', 'woocommerce' ) : __( 'Read more', 'woocommerce' );
+	}
+	return $text;
+}, 10 );
