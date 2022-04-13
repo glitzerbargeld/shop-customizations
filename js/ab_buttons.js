cb90685e49fa44cc878ed653f6c907a0b8506b96
glitzerbargeld <torben.jaeckel@me.com>
2022-04-13 11:24:09 +0200

    script fix

--- js/ab_buttons.js
+++ js/ab_buttons.js
@@ -1,15 +1,15 @@
-const sorte = document.querySelectorAll('.anbaumethode');
-const select = document.getElementById("pa_anbau");
-const dropdownselect = jQuery("#pa_anbau");
-const selectoptions = select.options;
-var selectoptionsarray = [];
+const ab_sorte = document.querySelectorAll('.anbaumethode');
+const ab_select = document.getElementById("pa_anbau");
+const ab_dropdownselect = jQuery("#pa_anbau");
+const ab_selectoptions = select.options;
+var ab_selectoptionsarray = [];
 
-for (var i = 0; i < select.options.length; i++) {
-    selectoptionsarray.push(select.options[i].value);
+for (var i = 0; i < ab_select.options.length; i++) {
+    ab_selectoptionsarray.push(ab_select.options[i].value);
 }
 
-sorte.forEach(el => {
-    if (selectoptionsarray.includes(el.getAttribute("data-el"))) {
+ab_sorte.forEach(el => {
+    if (ab_selectoptionsarray.includes(el.getAttribute("data-el"))) {
         el.parentElement.style.display = "block";
     }
     if (el.getAttribute("data-el") == select.value) {
@@ -20,21 +20,21 @@ sorte.forEach(el => {
 
 
 
-sorte.forEach(el => el.addEventListener('click', event => {
+ab_sorte.forEach(el => el.addEventListener('click', event => {
     event.preventDefault();
-    select.value = event.target.getAttribute("data-el");
-    dropdownselect.change();
-    sorte.forEach(el => el.style.backgroundColor = "gray");
+    ab_select.value = event.target.getAttribute("data-el");
+    ab_dropdownselect.change();
+    ab_sorte.forEach(el => el.style.backgroundColor = "gray");
     event.target.style.backgroundColor = "rgb(136, 175, 136)";
 
 
 }));
 
-sorte.forEach(el => el.addEventListener('touchstart', event => {
+ab_sorte.forEach(el => el.addEventListener('touchstart', event => {
     event.preventDefault();
-    select.value = event.target.getAttribute("data-el");
-    dropdownselect.change();
-    sorte.forEach(el => el.style.backgroundColor = "gray");
+    ab_select.value = event.target.getAttribute("data-el");
+    ab_dropdownselect.change();
+    ab_sorte.forEach(el => el.style.backgroundColor = "gray");
     event.target.style.backgroundColor = "rgb(136, 175, 136)";
 
 
