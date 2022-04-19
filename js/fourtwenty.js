5d50e68399b400b0193dd5566390f09a9641f9a4
glitzerbargeld <torben.jaeckel@me.com>
2022-04-19 17:47:56 +0200

    fourtwenty_4

--- js/fourtwenty.js
+++ js/fourtwenty.js
@@ -3,7 +3,12 @@ function copyToClipboard(element) {
     jQuery("body").append($temp);
     $temp.val(jQuery(element).text()).select();
     document.execCommand("copy");
-    jQuery('#fourtwenty').html("<p>Dein Gutscheindcode wurde kopiert! Einfach beim Bestellvorgang einfügen und die 20% sind dir sicher! &#128521; </p> <button aria-label=\"Close Account Info Modal Box\">×</button>");
+    jQuery('#fourtwenty').html("<p>Dein Gutscheindcode wurde kopiert! Einfach beim Bestellvorgang einfügen und die 20% sind dir sicher! &#128521; </p> <a id=\"close-btn\" aria-label=\"Close Account Info Modal Box\">X</a>");
+    document.getElementbyId("#close-btn").addEventListener("click", () => {
+        jQuery(element).styles.display = "none";
+    })
     $temp.remove();
   }
+
+
   
